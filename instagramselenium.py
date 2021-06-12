e7fddcf40395f7ad8f6e35fb761cd7068a2cc805
Enes Uysal <74737994+hulkienesuysal@users.noreply.github.com>
2021-06-12 21:37:28 +0300

    Add files via upload

--- instagramselenium.py
+++ instagramselenium.py
@@ -4,6 +4,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
+from peewee import Proxy,Model,CharField,SqliteDatabase
+
+db_proxy=Proxy()
+class BaseModel(Model):
+    class Meta:
+        database=db_proxy
+
+class Takipciler(BaseModel):
+    takipciler=CharField(null=True)
+class Takipettiklerim(BaseModel):
+    takipettiklerim=CharField(null=True)
+class Takipetmeyenler(BaseModel):
+    takipetmeyenler=CharField(null=True)
+class Takipetmediklerim(BaseModel):
+    takipetmediklerim=CharField(null=True)
+
+db = SqliteDatabase("instagram.db")
+db_proxy.initialize(db)
+
+db_proxy.connect()
+db_proxy.create_tables([Takipciler,Takipettiklerim,Takipetmeyenler,Takipetmediklerim], safe=True)
+
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 #driver=webdriver.Chrome(executable_path=r'C:\Users\chromedriver.exe')
@@ -23,10 +45,10 @@ while True:
             loginbutton=driver.find_element_by_xpath('//*[@id="loginForm"]/div[1]/div[3]/button').click()
         except:
             pass
-        
+
         sleep(sn+3)
         
-        driver.get('https://www.instagram.com/' + "username giriniz ") #username giriniz 
+        driver.get('https://www.instagram.com/' + "username giriniz") #username giriniz 
 
         followerscount=driver.find_element_by_xpath('/html/body/div[1]/section/main/div/header/section/ul/li[2]/a/span').text
         followersLink = driver.find_element_by_css_selector('ul li a').click()
@@ -51,6 +73,8 @@ while True:
             print(user)
             with open("instagram.txt", "a") as f:
                 f.write(user+"\n")
+            save=Takipciler.create(takipciler=user)
+            save.save()
             followers.append(user)
             if len(followers) == int(followerscount):  
                 followersout=driver.find_element_by_xpath('/html/body/div[5]/div/div/div[1]/div/div[2]/button').click()
@@ -75,12 +99,16 @@ while True:
             print(user)
             with open("instagram.txt", "a") as f:
                 f.write(user+"\n")
+            save=Takipettiklerim.create(takipettiklerim=user)
+            save.save()
             following.append(user)
 
         takipetmeyenler=[]
         for follow in following:
             if follow not in followers:
                 takipetmeyenler.append(follow)
+                save=Takipetmeyenler.create(takipetmeyenler=follow)
+                save.save()
 
         with open("instagram.txt", "a") as f:
             f.write("\n----Takip Etmeyenler----\n")
@@ -91,6 +119,8 @@ while True:
         for follow in followers:
             if follow not in following:
                 takipetmediklerim.append(follow)
+                save=Takipetmediklerim.create(takipetmediklerim=follow)
+                save.save()
 
         with open("instagram.txt", "a") as f:
             f.write("\n----Takip Etmediklerim----\n")
